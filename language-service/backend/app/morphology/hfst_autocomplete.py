2927a5341e76e28334f66cd16b8c1dff5a8e3220
William Lane <will@asklane.com>
2021-06-29 08:46:59 +0930

    python37

--- language-service/backend/app/morphology/hfst_autocomplete.py
+++ language-service/backend/app/morphology/hfst_autocomplete.py
@@ -10,15 +10,11 @@ class HFSTModel:
         self.fst = self.hfst_process.read()
         # print(help(self.fst))
 
-
     def apply_down(self, word):
-
         # do lookup
         # start = time.time()
         results = [x for x in self.fst.lookup(word, max_number=30)]
         results = [re.sub("\@.*?\@", "", x[0]) for x in results] # strip out flag diacritics
         # stop = time.time()
-        # print("LOOKUP: " + str(stop-start))
-
         return results
 
