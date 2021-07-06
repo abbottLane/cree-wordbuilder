a6e3d261c788e21eb72e19f50f86aca4d1f21dc2
William Lane <will@asklane.com>
2021-07-06 14:24:06 +0930

    take top n results in python

--- language-service/backend/app/morphology/hfst_autocomplete.py
+++ language-service/backend/app/morphology/hfst_autocomplete.py
@@ -1,6 +1,8 @@
 import hfst
 import re
 import time
+import pexpect
+import sys
 
 class HFSTModel:
     def __init__(self, modelPath):
@@ -9,13 +11,20 @@ class HFSTModel:
         hfst.set_default_fst_type(hfst.ImplementationType.HFST_OLW_TYPE)
         self.hfst_process = hfst.HfstInputStream(self.modelPath)
         self.fst = self.hfst_process.read()
+        self.num_results = 25
+       
 
     def apply_down(self, word):
         # do lookup
-        # start = time.time()
-        results = [x for x in self.fst.lookup(word, max_number=50)]
-        print("RESULT::::" + str(results))
+        start = time.time()
+        results = self.fst.lookup(word)[:self.num_results]
+        # print("RESULT::::" + str(results))
         results = [re.sub("\@.*?\@", "", x[0]) + " " + str(x[1]) for x in results] # strip out flag diacritics
-        # stop = time.time()
+        stop = time.time()
+        print("INFO: model query time: " + str(stop-start))
         return results
+
+
+       
+
 
